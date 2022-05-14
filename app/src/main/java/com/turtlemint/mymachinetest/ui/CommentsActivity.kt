0088fd1566937a7d0f5b796cbf43d6546de99501
Mohit <mhtparyani0@gmail.com>
2022-05-14 23:25:12 +0530

    Progress Bar added

--- app/src/main/java/com/turtlemint/mymachinetest/ui/CommentsActivity.kt
+++ app/src/main/java/com/turtlemint/mymachinetest/ui/CommentsActivity.kt
@@ -1,21 +1,23 @@
 package com.turtlemint.mymachinetest.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.turtlemint.mymachinetest.R
 import com.turtlemint.mymachinetest.data.CommentsList
-import com.turtlemint.mymachinetest.data.IssueList
 import com.turtlemint.mymachinetest.ui.adapter.CommentsAdapter
-import com.turtlemint.mymachinetest.ui.adapter.IssuesAdapter
 import com.turtlemint.mymachinetest.util.AppConstants
 import dagger.hilt.android.AndroidEntryPoint
 import de.hdodenhof.circleimageview.CircleImageView
+
 
 @AndroidEntryPoint
 class CommentsActivity : AppCompatActivity() {
@@ -30,16 +32,21 @@ class CommentsActivity : AppCompatActivity() {
     private lateinit var comments: TextView
     private lateinit var url: String
     private var issueId: Int= 0
+    private lateinit var progress: ProgressBar
     private val viewModel: CommentsViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comments)
+        val toolbar: Toolbar = findViewById<View>(R.id.my_toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val bundle= intent.extras
         titletv = findViewById(R.id.title)
         username = findViewById(R.id.user_name)
         desciption = findViewById(R.id.description)
         comments = findViewById(R.id.comments)
         avatar = findViewById(R.id.profile_image)
+        progress = findViewById(R.id.progress)
         if (bundle!=null){
             url= bundle.getString(AppConstants.COMM_URL,"")
             issueId= bundle.getInt(AppConstants.ISSUE_ID,0)
@@ -51,9 +58,18 @@ class CommentsActivity : AppCompatActivity() {
         setRecyclerView()
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId== android.R.id.home){
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun setRecyclerView() {
         recyclerView = findViewById(R.id.recycler_vew)
         errortv = findViewById(R.id.error)
+        recyclerView.visibility= View.GONE
+        progress.visibility= View.VISIBLE
         val commentsList: ArrayList<CommentsList> = ArrayList<CommentsList>()
         commentsAdapter = CommentsAdapter(this, commentsList)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -76,6 +92,7 @@ class CommentsActivity : AppCompatActivity() {
                 errortv.visibility= View.VISIBLE
                 comments.visibility= View.GONE
             }
+            progress.visibility= View.GONE
         }
 
     }
--- app/src/main/java/com/turtlemint/mymachinetest/ui/IssuesActivity.kt
+++ app/src/main/java/com/turtlemint/mymachinetest/ui/IssuesActivity.kt
@@ -2,6 +2,7 @@ package com.turtlemint.mymachinetest.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +19,7 @@ class IssuesActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var errortv: TextView
     private lateinit var issuesAdapter: IssuesAdapter
+    private lateinit var progress: ProgressBar
     private val viewModel: IssueListViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +29,10 @@ class IssuesActivity : AppCompatActivity() {
 
     private fun setRecyclerView() {
         recyclerView = findViewById(R.id.recycler_vew)
+        progress = findViewById(R.id.progress)
         errortv = findViewById(R.id.error)
+        recyclerView.visibility= View.GONE
+        progress.visibility= View.VISIBLE
         val notificationModelArrayList: ArrayList<IssueList> = ArrayList<IssueList>()
         issuesAdapter = IssuesAdapter(this, notificationModelArrayList)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -47,6 +52,7 @@ class IssuesActivity : AppCompatActivity() {
                 recyclerView.visibility= View.GONE
                 errortv.visibility= View.VISIBLE
             }
+            progress.visibility= View.GONE
         }
 
     }
